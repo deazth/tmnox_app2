6fbb73cbd13fa072cb07ceb1d5cadee6b8765c17
deazth <deazth@gmail.com>
2017-07-21 12:28:56 +0800

    added the rest of the stuffs

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,43 +1,194 @@
-// Initialize app
+// Initialize your app
 var myApp = new Framework7();
 
-
-// If we need to use custom DOM library, let's save it to $$ variable:
+// Export selectors engine
 var $$ = Dom7;
+
+// global vars
+var SearchParam = {};
+var baInfo = {};
+var sblActivity = {};
+var swiftActivity = {}; 
+var contentBlock;
+var mainURL = 'http://localhost:8080/NoxMW';
 
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
+    // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+// page inits
+myApp.onPageInit('bill_summ', function (page) {
+  document.getElementById("billsumpt").innerHTML = "BA#" + SearchParam.banumber;
+  baLoadAccInfo("ba_summ_block");
+});  
+
+myApp.onPageInit('order_summ', function (page) {
+  document.getElementById("orderSumTopTitle").innerHTML = "Order: " + SearchParam.ordernumber;
+  soLoadSiebelActivity("siebel_item_lst");
 });
 
+myApp.onPageInit('ba_trial_bill', function (page){
+  document.getElementById("trialbillpt").innerHTML = "Trial Bill: " + SearchParam.banumber;
+});
 
-// Now we need to run the code that will be executed only for About page.
+myApp.onPageInit('ba_sr_summary', function (page){
+  document.getElementById("basrpt").innerHTML = "SR Summary: " + SearchParam.banumber;
+  baLoadAccInfo("sr_summ_block");
 
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
+  // Pull to refresh content
+    var ptrContent = $$(page.container).find('.pull-to-refresh-content');
+    // Add 'refresh' listener on it
+    ptrContent.on('refresh', function (e) {
+        // reload current page
+        mainView.router.reloadPage("ba_sr_summary.html");
+        myApp.pullToRefreshDone();
+    });
 
-})
+});
 
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
+myApp.onPageInit('ba_h_pymt', function (page){
+  document.getElementById("pymthpt").innerHTML = "Payment History: " + SearchParam.banumber;
 
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+  // Pull to refresh content
+    var ptrContent = $$(page.container).find('.pull-to-refresh-content');
+    // Add 'refresh' listener on it
+    ptrContent.on('refresh', function (e) {
+        // reload current page
+        mainView.router.reloadPage("ba_h_pymt.html");
+        myApp.pullToRefreshDone();
+    });
+});
+
+// search functions
+function searchBA(){
+  SearchParam = myApp.formToData('#ba-s-form');
+  SearchParam.sType = "searchBA";
+//  alert(SBA_SearchParam.banumber);
+
+  var baUrl = mainURL + '/brm/get_acc_info.jsp?bano=' + SearchParam.banumber;
+
+  //myApp.alert('Searching for ' + SearchParam.banumber, 'Ouch');
+
+
+
+  $.getJSON(baUrl, function(baInfo){
+  
+    contentBlock = 
+        '<p>Name: ' + baInfo.name + '</p>' +
+        '<p>Bill Cycle: ' + baInfo.bp + '</p>' +
+        '<p>Bill Media: ' + baInfo.billmedia + '</p>' +
+        '<p>Address: ' + baInfo.address + '</p>' +
+        '<p>To Email: ' + baInfo.toemail + '</p>' +
+        '<p>CC Email: ' + baInfo.ccemail + '</p>' +
+        '<p>Mobile No: ' + baInfo.mobileno + '</p>' +
+        '<p>Current Outstanding: ' + baInfo.outstanding + '</p>';
+        mainView.loadPage("ba_summary.html");
+        
+        myApp.alert('Data fetcged ' + SearchParam.banumber, 'Ouch');
+  })
+    .fail(function(){
+      myApp.alert('Unable to fetch data for ' + SearchParam.banumber, 'Ouch');
+    });
+
+    
+
+// get the BA info
+  // baInfo = {
+  //   'name':'Siti Pelanggan Binti Abahnye',
+  //   'mobileno':'019-3652001',
+  //   'billmedia':'0',
+  //   'bp':'10',
+  //   'address':'22|baker street|Tronoh, Kolo Piloh',
+  //   'toemail':'abu@websitekoi.kom',
+  //   'ccemail':'',
+  //   'outstanding':'315.50'
+  // };
+
+  
+}
+
+function searchOrder(){
+  SearchParam = myApp.formToData('#order-s-form');
+  SearchParam.sType = "searchOrder";
+//  alert(SBA_SearchParam.banumber);
+
+  sblActivity = {'activities':[
+    {
+      'number':'1-45623125',
+      'status':'processing',
+      'name':'sample order 1'
+    },
+    {
+      'number':'1-45623126',
+      'status':'done',
+      'name':'sample order 2'
+    },
+    {
+      'number':'1-45623127',
+      'status':'pending',
+      'name':'sample order 3'
     }
-})
+  ]};
 
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
+  mainView.loadPage("order_summary.html");
+}
+
+// Billing related functions
+function getTrialBill(){
+  SearchParam = myApp.formToData('#order-s-form');
+  SearchParam.sType = "searchOrder";
+//  alert(SBA_SearchParam.banumber);
+
+  mainView.loadPage("order_summary.html");
+}
+
+function baLoadAccInfo(blockname){
+  document.getElementById(blockname).innerHTML = contentBlock;
+}
+
+function soLoadSiebelActivity(blockname){
+  var sblAContent = '<ul>';
+  var itemcount = 0;
+  for(act in sblActivity.activities){
+    itemcount = itemcount + 1;
+    var sblItem = 
+      '<li class="item-content">' +
+        '<div class="item-inner">' +
+          '<div class="item-title-row">' +
+            '<div class="item-title">' + sblActivity.activities[act].number + '</div>' +
+            '<div class="item-after">' + sblActivity.activities[act].name + '</div>' +
+            '<div class="item-after">' + sblActivity.activities[act].status + '</div>' +
+          '</div>' +
+        '</div>' +
+      '</li>';
+
+      sblAContent = sblAContent + sblItem;
+  }
+
+  if(itemcount == 0){
+    '<li class="item-content">' +
+        '<div class="item-inner">' +
+          '<div class="item-title-row">' +
+            '<div class="item-title">No activity found</div>' +
+          '</div>' +
+        '</div>' +
+      '</li>';
+
+      sblAContent = sblAContent + sblItem;
+  }
+
+  sblAContent = sblAContent + '</ul>';
+
+  document.getElementById(blockname).innerHTML = sblAContent;
+
+}
+
+function cHttpGet(theURL){
+  var Httpreq = new XMLHttpRequest(); // a new request
+  Httpreq.open("GET",theURL,false);
+  Httpreq.send(null);
+  return Httpreq.responseText;   
+}
+
